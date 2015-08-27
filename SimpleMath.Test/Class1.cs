9a46a0a5063ccd2ce5a25b148c6d33d92acdedb4
DexteriousDevengers <vijay.kumar.baskaran@verizon.com>
2015-08-27 06:55:22 -0700

    check in

---
+++ SimpleMath.Test/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace SimpleMath.Test
+{
+    [TestFixture]
+    //Test Code
+    public class GitTesting1Test
+    {
+        [TestCase]
+        public void AddTest()
+        {
+            SimpleMath.MathsHelper helper = new SimpleMath.MathsHelper();
+            int result = helper.Add(20, 10);
+            Assert.AreEqual(30, result);
+        }
+        [TestCase]
+        public void SubtractTest()
+        {
+            SimpleMath.MathsHelper helper = new SimpleMath.MathsHelper();
+            int result = helper.Subtract(20, 10);
+            Assert.AreEqual(10, result);
+        }
+    }
+}
